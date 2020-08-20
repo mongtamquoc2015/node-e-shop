97074fc5120c61bafd0770192fa8d68f0763a0f3
Namisan <mongtamquoc2015@gmail.com>
2020-08-20 10:00:59 +0700

    Write a simple CRUD api following the RESTful standard with nodejs express and mongoose

--- api/controllers/productApi.controller.js
+++ api/controllers/productApi.controller.js
@@ -1,6 +1,25 @@
-const Products = require('../../models/product.model');
+const Product = require('../../models/product.model');
 
 module.exports.index = async (req, res) => {
-	const products = await Products.find();
+	const products = await Product.find();
 	res.json(products);
+}
+
+module.exports.store = async (req, res) => {
+	const newProduct = await Product.create(req.body);
+	res.json(newProduct);
+}
+
+module.exports.update = async (req, res, next) => {
+	const id = req.params.id;
+	await Product.findByIdAndUpdate(id, { $set: req.body }, (err, product) => {
+		if (err) return next(err);
+		res.send('Product updated');
+	});
+}
+
+module.exports.destroy = async (req, res) => {
+	const id = req.params.id;
+	await Product.remove({ _id: id });
+	res.send('Product deleted');
 }
--- api/routes/api.route.js
+++ api/routes/api.route.js
@@ -2,5 +2,8 @@ const router = require('express').Router();
 const productApiController = require('../controllers/productApi.controller');
 
 router.get('/products', productApiController.index);
+router.post('/products', productApiController.store);
+router.patch('/products/:id', productApiController.update);
+router.delete('/products/:id', productApiController.destroy);
 
 module.exports = router;
