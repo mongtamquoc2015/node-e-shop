7d6c6e94d4428d3da18fcdb3d3ae1a11c648a152
Namisan <mongtamquoc2015@gmail.com>
2020-08-20 12:16:01 +0700

    Fix try-catch

--- controllers/home/home.controller.js
+++ controllers/home/home.controller.js
@@ -1,12 +1,21 @@
 const Product = require('../../models/product.model');
+const { NotExtended } = require('http-errors');
 
 
-module.exports.getAllProducts = async (req, res) => {
-	const products = await Product.find();
-	res.render('home/pages/products', {products: products});
+module.exports.getAllProducts = async (req, res, next) => {
+	try {
+		const products = await Product.find();
+		res.render('home/pages/products', { products: products });
+	} catch (err) {
+		next(err);
+	}
 }
 
-module.exports.getProductHighlights = async (req, res) => {
-	const products = await Product.find()
-	res.render('home/pages/index', {products: products.slice(0,24)});
+module.exports.getProductHighlights = async (req, res, next) => {
+	try {
+		const products = await Product.find()
+		res.render('home/pages/index', { products: products.slice(0, 24) });
+	} catch (err) {
+		next(err);
+	}
 }
--- controllers/home/user.controller.js
+++ controllers/home/user.controller.js
@@ -1,45 +1,54 @@
 const User = require('../../models/user.model');
+const { NotExtended } = require('http-errors');
 
 
-module.exports.showLoginForm = (req,res) => {
-	res.render('home/pages/login', {title: 'Login'});
+module.exports.showLoginForm = (req, res, next) => {
+	res.render('home/pages/login', { title: 'Login' });
 }
 
-module.exports.attempsUserLogin = async (req,res) => {
-	const email = req.body.email;
-	const password = req.body.password;
-	const user = await User.findOne({email: email, password: password});
-	if (user) {
-		res.cookie('userId',user.id, {
-			signed: true
-		});
-		res.redirect('/');
+module.exports.attempsUserLogin = async (req, res) => {
+	try {
+		const email = req.body.email;
+		const password = req.body.password;
+		const user = await User.findOne({ email: email, password: password });
+		if (user) {
+			res.cookie('userId', user.id, {
+				signed: true
+			});
+			res.redirect('/');
+		}
+		res.render('home/pages/login', { title: 'Login', errors: ["Email or password is not correct."] });
+	} catch (err) {
+		next(err);
 	}
-	res.render('home/pages/login', { title: 'Login', errors: ["Email or password is not correct."]});
 }
 
 module.exports.showSigninForm = (req, res) => {
-	res.render('home/pages/signin', { title: 'Signin'});
+	res.render('home/pages/signin', { title: 'Signin' });
 }
 
-module.exports.createUser = (req,res) => {
-	const name = req.body.name;
-	const email = req.body.email;
-	const password = req.body.password;
-	let avatar = req.body.avatar;
-	avatar = req.file.path.split('/').slice(1).join('/');
+module.exports.createUser = async (req, res, next) => {
+	try {
+		const name = req.body.name;
+		const email = req.body.email;
+		const password = req.body.password;
+		let avatar = req.body.avatar;
+		avatar = req.file.path.split('/').slice(1).join('/');
 
-	if (!name || !email || !password) {
-		res.render('home/pages/signin', {errors: ["Fields is require"]});
+		if (!name || !email || !password) {
+			res.render('home/pages/signin', { errors: ["Fields is require"] });
+		}
+
+		const user = {
+			name: name,
+			email: email,
+			password: password,
+			avatar: avatar
+		};
+		await User.insertMany([user]);
+
+		res.render('home/pages/login', { title: 'Login' });
+	} catch (err) {
+		next(err);
 	}
-
-	const user = {
-		name: name,
-		email: email,
-		password: password,
-		avatar: avatar
-	};
-	User.insertMany([user]);
-
-	res.render('home/pages/login', { title: 'Login' });
 }
